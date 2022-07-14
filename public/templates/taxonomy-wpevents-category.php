996888b7dc7b3d20976215d73c373022180acda5
Noor ul ain <noor@wpminds.com>
2022-07-14 19:08:27 +0500

    WP Events free version

---
+++ public/templates/taxonomy-wpevents-category.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * wp_events archive page
+ */
+
+get_header();
+?>
+
+<div class="wpe-event">
+    <div class="wpe-full-wrap">
+        <div class="wpevents-container">
+            <?php
+            wpe_get_taxonomy_page_title();
+
+            $args  = wpe_get_tex_query_args();
+            $count = wpe_display_archive_posts( $args );
+
+            if ( $count == 0 ) {                
+	            /**
+	             * Print the subscriber form if no event is added
+	             * or all events are over due
+	             *
+	             * @since  1.0.449
+	             * @action wpe_display_subscribe_form
+	             */
+	            do_action( 'wp_events_subscribe_form' );          // Displays Subscribe Form
+            }
+            wp_reset_postdata();
+            ?>
+        </div>
+    </div>
+</div>
+<?php
+get_footer();
+?>
